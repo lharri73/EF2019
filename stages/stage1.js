6d3d8417fc3d4c989b4eabc4df6d215362506744
Landon <lharri73@vols.utk.edu>
2019-03-19 16:07:37 -0500

    added structure for instructions

--- stages/stage1.js
+++ stages/stage1.js
@@ -7,6 +7,7 @@ function stage1Constructor() {
   );
   targetPosition = createVector(windowWidth - 50, initialPosition.y);
   isActive = false;
+  instructionStage = 0;
   thisBall = new ball(initialPosition, 0, 0);
 
   //this creates the textBox in the middle of the screen
@@ -23,9 +24,18 @@ function stage1Constructor() {
 function drawStage1() {
   //this is where the code for stage one will live
   //once completed, run function incrimentStage()
-  drawTarget(targetPosition.x, targetPosition.y);
-  thisBall.update();
-  thisBall.draw();
+  switch (instructionStage) {
+    case 0:
+      drawStage1Instructions();
+      break;
+    case 1:
+      break;
+    default:
+      drawTarget(targetPosition.x, targetPosition.y);
+      thisBall.update();
+      thisBall.draw();
+      break;
+  }
 }
 
 function stage1keyPressed(value) {
@@ -46,4 +56,23 @@ function stage1keyPressed(value) {
 
 function stage1MouseClicked() {
   //executed when on stage one and the mouse is clicked
+}
+
+function drawStage1Instructions() {
+  noFill();
+  stroke(0);
+  strokeWeight(3);
+  var border = 10;
+  var width = 500;
+  var height = 300;
+  var positionX = windowWidth / 2 - width / 2;
+  var positionY = windowHeight / 2 - height / 2;
+  rect(positionX, positionY, width, height);
+  strokeWeight(1); //reset stroke weight to default
+  fill(0); //reset fill for text
+  text(
+    "Hello, this is test Text",
+    positionX + border,
+    positionY + textAscent() + border
+  );
 }
