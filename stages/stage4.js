8c9ec177d3c7d9c5aec5ef0c87816544952e59eb
Landon <lharri73@vols.utk.edu>
2019-03-28 14:37:25 -0400

    fixed stage 4 NaN

--- stages/stage4.js
+++ stages/stage4.js
@@ -10,7 +10,7 @@ function stage4Constructor() {
   );
   targetPosition = createVector(
     windowWidth - 50,
-    roundToFixed(random(ballPosition.y - 100, windowHeight - 80), 2)
+    roundToFixed(random(ballPosition.y, windowHeight - 80), 2)
   );
 
   thisTarget = new target(targetPosition, 19);
